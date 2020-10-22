2eecc0aed54d5a9b8dc917d05008af6994beec51
Jeff Lucas <jlucas13@eil-server.cba.ua.edu>
2020-10-22 08:54:50 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace class10_21_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dog myDog = new Dog();
+            // myDog.SetName("Sofie");
+            // myDog.SetBreed("Rat Terrier");
+            // myDog.SetWeight(15);
+            // myDog.Bark();
+
+            // Exercise(myDog);
+            // Console.WriteLine(myDog.GetWeight());
+
+            Dog[] myDogs = GetAllDogs();
+            PrintAllDogs(myDogs);
+            
+        }
+
+        public static void Exercise(Dog localDog) {
+            localDog.SetWeight(localDog.GetWeight()-1);
+           
+        }
+
+        public static Dog[] GetAllDogs() {
+            Dog[] myDogs = new Dog[100];
+            Dog.SetCount(0);
+            string userInput = "";
+            Console.WriteLine("Enter dog's name, stop to quit: ");
+            userInput=Console.ReadLine();
+            while(userInput.ToUpper() != "STOP") {
+                Dog temp = new Dog();
+                temp.SetName(userInput);
+                Console.WriteLine("Enter the breed: ");
+                temp.SetBreed(Console.ReadLine());
+                Console.WriteLine("Enter the weight: ");
+
+                temp.SetWeight(int.Parse(Console.ReadLine()));
+                myDogs[Dog.GetCount()] = temp;
+                Dog.IncCount();
+
+                Console.WriteLine("Enter dog's name, stop to quit: ");
+                userInput=Console.ReadLine();
+            }
+
+            return myDogs;
+
+        }
+
+        public static void PrintAllDogs(Dog[] myDogs) {
+            for(int i=0; i < Dog.GetCount(); i++) {
+                Console.WriteLine(myDogs[i].ToString());
+            }
+        }
+    }
+}
